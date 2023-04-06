02979a4dde32315c510e83b71b67f0ecc99cee6b
agwaBom <khyunjin1993@gmail.com>
2023-04-06 16:30:15 +0900

    initialized repository with skeleton code

---
+++ main.py
@@ -0,0 +1,16 @@
+from typing import List
+
+def path_to_file_list(path: str) -> List[str]:
+    """Reads a file and returns a list of lines in the file"""
+    pass
+
+def train_file_list_to_json(english_file_list: List[str], german_file_list: List[str]) -> List[str]:
+    """Converts two lists of file paths into a list of json strings"""
+    pass
+
+def write_file_list(file_list: List[str], path: str) -> None:
+    """Writes a list of strings to a file, each string on a new line"""
+    pass
+
+if __name__ == "__main__":
+    pass
