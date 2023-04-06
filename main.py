1c53845dd0f27db0e93c13c019038f40a5ab38f2
agwaBom <khyunjin1993@gmail.com>
2023-04-06 16:30:50 +0900

    main code finalized

--- main.py
+++ main.py
@@ -13,4 +13,13 @@ def write_file_list(file_list: List[str], path: str) -> None:
     pass
 
 if __name__ == "__main__":
-    pass
+    path = './'
+    german_path = './german.txt'
+    english_path = './english.txt'
+
+    english_file_list = path_to_file_list(english_path)
+    german_file_list = path_to_file_list(german_path)
+
+    processed_file_list = train_file_list_to_json(english_file_list, german_file_list)
+
+    write_file_list(processed_file_list, path+'concated.json')
