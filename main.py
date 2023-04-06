061269e28160a0eda39a815e04368664149d3fa0
agwaBom <khyunjin1993@gmail.com>
2023-04-06 16:36:41 +0900

    path_to_file_list implemented

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from typing import List
 
 def path_to_file_list(path: str) -> List[str]:
     """Reads a file and returns a list of lines in the file"""
-    pass
+    # implemented as a one-liner
+    lines = open(path, 'w').split('\n')
+    return lines
 
 def train_file_list_to_json(english_file_list: List[str], german_file_list: List[str]) -> List[str]:
     """Converts two lists of file paths into a list of json strings"""
@@ -13,7 +15,7 @@ def write_file_list(file_list: List[str], path: str) -> None:
     with open(path, 'w') as f:
         for file in file_list:
             f.write(file + '\n')
-
+            
 if __name__ == "__main__":
     path = './'
     german_path = './german.txt'
